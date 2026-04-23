bd896dd1ead39f9a48f7cbf4945acadc79e21b2b
1st update <gavim0009@gmail.com>
2026-04-23 10:54:18 +0530

    chore: setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dsavisualizer/',
   plugins: [react()],
   server: {
     proxy: {
